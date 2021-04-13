a05dd8993c0f195e91893b51ad4d7e94ad1baf0c
Victor-Mateus-Ferreira <victormateuslf@gmail.com>
2021-04-13 11:02:38 -0300

    install cors

--- back-end/index.js
+++ back-end/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const registerController = require('./Controller/registerController');
 const bodyParser = require('body-parser');
 
-
-
 const app = express();
-const PORT = 3001;
+const PORT = 8001;
+app.use(cors());
+
 app.use(bodyParser.json());
 
 
